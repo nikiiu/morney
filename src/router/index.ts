9cf0a386e2df170eec6ab79e67b1f0671f0bb076
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-07-17 21:09:57 +0800

    添加40404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import Home from "@/views/Home.vue";
 import Money from "@/views/Money.vue";
 import Labels from "@/views/Labels.vue";
 import Statistics from "@/views/Statistics.vue";
-
+import NotFound from "@/views/NotFound.vue";
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
@@ -23,6 +23,10 @@ const routes: Array<RouteConfig> = [
   {
     path: "/statistics",
     component: Statistics,
+  },
+  {
+    path: "*",
+    component: NotFound,
   },
 ];
 
