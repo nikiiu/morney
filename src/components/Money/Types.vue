202e5272281f0d950b9a34b6366d94e05507b7ef
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-07-28 17:06:51 +0800

    模块化：把Money分成4个小组件

---
+++ src/components/Money/Types.vue
@@ -0,0 +1,38 @@
+<template>
+        <ul class="types">
+            <li class="selected">支出</li>
+            <li>收入</li>
+        </ul>
+</template>
+
+<script>
+    export default {
+        name: 'Types'
+    }
+</script>
+
+<style lang="scss" scoped>
+ .types{
+    background: #c4c4c4;
+    display: flex;
+    text-align: center;
+    font-size: 24px;
+    > li {
+        width: 50%;
+        height: 64px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        &.selected::after{//当前li被选中
+            content: '';
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 4px;
+            background: #333;
+        }
+    }
+  }
+</style>
