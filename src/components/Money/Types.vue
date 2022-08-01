a1dcf28b83959219bdf78f82834d225545c94725
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-08-01 17:27:33 +0800

    使用JS实现了一个组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,13 +1,32 @@
 <template>
         <ul class="types">
-            <li class="selected">支出</li>
-            <li>收入</li>
+            <li :class="type === '-' && 'selected'" 
+                @click="selectType('-')">支出</li>
+            <li :class="type === '+' && 'selected'" 
+                @click="selectType('+')">收入</li>
         </ul>
 </template>
 
 <script>
     export default {
-        name: 'Types'
+        name: 'Types',
+        props: ['xxx'],
+        data() {
+            return{
+                type:'-' // '-'表示收入， '+'表示支出
+            }
+        },
+        mounted() {
+            console.log(this.xxx)
+        },
+        methods: {
+            selectType(type) { //type只能是+或-
+                if(type !== '-' && type !== '+'){
+                    throw new Error('type is unknown')
+                }
+                this.type = type
+            }
+        }
     }
 </script>
 
