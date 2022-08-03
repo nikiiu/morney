ceedda867d70229f45748d28aaf9b6b66f23137c
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-08-03 23:07:23 +0800

    完成 notes 组件

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,33 +1,46 @@
 <template>
-    <label class="notes">
-        <span class="name">备注</span>
-        <input type="text" placeholder="在这里输入备注">
-    </label>
+  <label class="notes">
+    {{ value }}
+    <span class="name">备注</span>
+    <input
+      type="text"
+      :value="value"
+      @input="onInput"
+      placeholder="在这里输入备注"
+    />
+  </label>
 </template>
 
-<script>
-    export default {
-        name: 'Notes'
-    }
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+@Component
+export default class Notes extends Vue {
+  value = "";
+  onInput(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    this.value = input.value;
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-.notes{
-    font-size: 14px;
-    background: #f5f5f5;
-    display: block;
-    padding-left: 16px;
-    display: flex;
-    align-items: center;
-    .name{
-        padding-right: 16px;
-    }
-    input{
-        height: 64px;
-        flex-grow: 1;
-        background: transparent;
-        border: none;
-        padding-right: 16px;
-    }
+.notes {
+  font-size: 14px;
+  background: #f5f5f5;
+  display: block;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+  .name {
+    padding-right: 16px;
   }
+  input {
+    height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
 </style>
