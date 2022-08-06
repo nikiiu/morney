df3e729122db39496c5c4f23afc48547e2f1b337
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-08-06 20:23:24 +0800

    把 record 的复制保存至 list

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -57,6 +57,7 @@ export default class NumberPad extends Vue {
   }
   ok() {
     this.$emit("update:value", this.output);
+    this.$emit("submit", this.output);
   }
 }
 </script>
@@ -66,7 +67,7 @@ export default class NumberPad extends Vue {
 .numberPad {
   .output {
     @extend %clearFix;
-    @extend %innerShadow;
+
     font-size: 36px;
     font-family: Consolas, monospace;
     padding: 9px 16px;
