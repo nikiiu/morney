fed131069f16f6f3b897da1a5be597f146a4bff6
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-08-11 17:42:59 +0800

    展示和创建标签

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <router-view/>
-    </div>
+    <router-view />
+  </div>
 </template>
 
 <style lang="scss">
 @import "~@/assets/style/reset.scss";
 @import "~@/assets/style/helper.scss";
-#app {
+body {
   line-height: 1.5;
   font-family: $font-hei;
   font-size: 16px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #333;
+  background: #f5f5f5;
 }
-
 </style>
