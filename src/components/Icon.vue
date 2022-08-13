3336d2a7aae7121d075da20fb25b70e1707a15d3
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-08-13 11:50:59 +0800

    可以编辑和删除标签

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,25 +1,30 @@
 <template>
-    <svg class="icon">
-        <use v-bind:xlink:href="'#'+name"/>
-    </svg>
+  <svg class="icon" @click="$emit('click', $event)">
+    <use v-bind:xlink:href="'#' + name" />
+  </svg>
 </template>
 
 <script lang="ts">
 // import一个目录,context()是指定要去哪个目录搜索
-    let importAll = (requireContext:__WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
-    try{importAll(require.context('../assets/icons',true,/\.svg$/));}catch(error){console.log(error);}
-    export default {
-        props: ['name'],
-        name: 'Icon'
-    }
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) =>
+  requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context("../assets/icons", true, /\.svg$/));
+} catch (error) {
+  console.log(error);
+}
+export default {
+  props: ["name"],
+  name: "Icon",
+};
 </script>
 
 <style lang="scss" scoped>
-    .icon{
-        width: 1em; //1em是一个字
-        height: 1em;
-        vertical-align: -0.15em;
-        fill: currentColor;
-        overflow: hidden;
-    }
+.icon {
+  width: 1em; //1em是一个字
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
 </style>
