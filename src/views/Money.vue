1fdd6a23b54260807c663ffff7cbce1bf2039d8f
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-08-14 11:33:11 +0800

    将所有数字操作封装在store里

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -20,13 +20,14 @@ import Types from "../components/Money/Types.vue";
 import FormItem from "../components/Money/FormItem.vue";
 import Tags from "../components/Money/Tags.vue";
 import { Component } from "vue-property-decorator";
+import store from "@/store/index2";
 
 @Component({
   components: { NumberPad, Types, FormItem, Tags },
 })
 export default class Money extends Vue {
-  tags = window.tagList;
-  recordList = window.recordList;
+  tags = store.tagList;
+  recordList = store.recordList;
   record: RecordItem = {
     tags: [],
     notes: "",
@@ -43,7 +44,7 @@ export default class Money extends Vue {
     this.record.amount = parseFloat(value);
   }
   saveRecord() {
-    window.createRecord(this.record);
+    store.createRecord(this.record);
   }
 }
 </script>
