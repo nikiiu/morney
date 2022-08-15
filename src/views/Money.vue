fbe2cd90fca4a2a5721bc1b8cd5c020cf15b81d9
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-08-15 17:18:18 +0800

    tag保存有bug

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -9,7 +9,7 @@
         placeholder="在这里输入备注"
       />
     </div>
-    <Tags :data-source.sync="tags" @update:value="onUpdateTags" />
+    <Tags />
   </Layout>
 </template>
 
@@ -20,23 +20,19 @@ import Types from "../components/Money/Types.vue";
 import FormItem from "../components/Money/FormItem.vue";
 import Tags from "../components/Money/Tags.vue";
 import { Component } from "vue-property-decorator";
-import store from "@/store/index2";
+import store2 from "@/store/index2";
 
 @Component({
   components: { NumberPad, Types, FormItem, Tags },
 })
 export default class Money extends Vue {
-  tags = store.tagList;
-  recordList = store.recordList;
+  recordList = store2.recordList;
   record: RecordItem = {
     tags: [],
     notes: "",
     type: "+",
     amount: 0,
   };
-  onUpdateTags(value: string[]) {
-    this.record.tags = value;
-  }
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
@@ -44,7 +40,7 @@ export default class Money extends Vue {
     this.record.amount = parseFloat(value);
   }
   saveRecord() {
-    store.createRecord(this.record);
+    store2.createRecord(this.record);
   }
 }
 </script>
