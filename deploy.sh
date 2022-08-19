1400fd664066c889be9c9e0eca2da557bef6eb1c
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-08-19 23:35:19 +0800

    优化PC界面的样式

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,5 @@ git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:nikiiu/morney-website.git master:gh-pages
-
+git push -f git@gitee.com:nikyu77/morney-website.git master:gh-pages
 cd -
