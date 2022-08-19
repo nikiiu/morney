a9490a5acfeb82d730dfbd890dc6b8ba9d4589d8
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-08-19 22:32:59 +0800

    部署网站到 morney-website

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,10 @@ const path = require("path");
 
 
 module.exports = {
-  publicPath:'',
+  publicPath: 
+  process.env.NODE_ENV === 'production'
+  ? '/morney-website/'
+  : '/',
   lintOnSave: false,
   chainWebpack: (config) => {
     const dir = path.resolve(__dirname, "src/assets/icons");
