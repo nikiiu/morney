87222f006dc461c0c8a48f25220789041032268a
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-08-24 23:25:02 +0800

    当金额为0时无法记账

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -57,6 +57,9 @@ export default class NumberPad extends Vue {
     this.output = "0";
   }
   ok() {
+    if (this.output === "0") {
+      return window.alert("请输入金额");
+    }
     this.$emit("update:value", parseFloat(this.output));
     this.$emit("submit", parseFloat(this.output));
     this.output = "0";
